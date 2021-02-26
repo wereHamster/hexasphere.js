d217f3b1ffd92d52401b549060eaab917f701e7d
Tomas Carnecky <tomc@caurea.org>
2021-02-26 14:14:16 +0000

    ...

--- src/tile.js
+++ src/tile.js
@@ -32,91 +32,82 @@ function pointingAwayFromOrigin(p, v){
     return ((p.x * v.x) >= 0) && ((p.y * v.y) >= 0) && ((p.z * v.z) >= 0)
 }
 
-var Tile = function(centerPoint, hexSize){
-    
-    if(hexSize == undefined){
-        hexSize = 1;
-    }
+export class Tile {
 
-    hexSize = Math.max(.01, Math.min(1.0, hexSize));
+    constructor (centerPoint, hexSize = 1){
+        hexSize = Math.max(.01, Math.min(1.0, hexSize));
 
-    this.centerPoint = centerPoint;
-    this.faces = centerPoint.getOrderedFaces();
-    this.boundary = [];
-    this.neighborIds = []; // this holds the centerpoints, will resolve to references after
-    this.neighbors = []; // this is filled in after all the tiles have been created
+        this.centerPoint = centerPoint;
+        this.faces = centerPoint.getOrderedFaces();
+        this.boundary = [];
+        this.neighborIds = []; // this holds the centerpoints, will resolve to references after
+        this.neighbors = []; // this is filled in after all the tiles have been created
 
-    var neighborHash = {};
-    for(var f=0; f< this.faces.length; f++){
-        // build boundary
-        this.boundary.push(this.faces[f].getCentroid().segment(this.centerPoint, hexSize));
+        var neighborHash = {};
+        for(var f=0; f< this.faces.length; f++){
+            // build boundary
+            this.boundary.push(this.faces[f].getCentroid().segment(this.centerPoint, hexSize));
 
-        // get neighboring tiles
-        var otherPoints = this.faces[f].getOtherPoints(this.centerPoint);
-        for(var o = 0; o < 2; o++){
-            neighborHash[otherPoints[o]] = 1;
+            // get neighboring tiles
+            var otherPoints = this.faces[f].getOtherPoints(this.centerPoint);
+            for(var o = 0; o < 2; o++){
+                neighborHash[otherPoints[o]] = 1;
+            }
+
         }
 
+        this.neighborIds = Object.keys(neighborHash);
+
+        // Some of the faces are pointing in the wrong direction
+        // Fix this.  Should be a better way of handling it
+        // than flipping them around afterwards
+
+        var normal = calculateSurfaceNormal(this.boundary[1], this.boundary[2], this.boundary[3]);
+
+        if(!pointingAwayFromOrigin(this.centerPoint, normal)){
+            this.boundary.reverse();
+        }
     }
 
-    this.neighborIds = Object.keys(neighborHash);
-
-    // Some of the faces are pointing in the wrong direction
-    // Fix this.  Should be a better way of handling it
-    // than flipping them around afterwards
-
-    var normal = calculateSurfaceNormal(this.boundary[1], this.boundary[2], this.boundary[3]);
-
-    if(!pointingAwayFromOrigin(this.centerPoint, normal)){
-        this.boundary.reverse();
+    getLatLon(radius, boundaryNum){
+        var point = this.centerPoint;
+        if(typeof boundaryNum == "number" && boundaryNum < this.boundary.length){
+            point = this.boundary[boundaryNum];
+        }
+        var phi = Math.acos(point.y / radius); //lat 
+        var theta = (Math.atan2(point.x, point.z) + Math.PI + Math.PI / 2) % (Math.PI * 2) - Math.PI; // lon
+        
+        // theta is a hack, since I want to rotate by Math.PI/2 to start.  sorryyyyyyyyyyy
+        return {
+            lat: 180 * phi / Math.PI - 90,
+            lon: 180 * theta / Math.PI
+        };
     }
 
 
+    scaledBoundary(scale){
+        scale = Math.max(0, Math.min(1, scale));
 
-};
+        var ret = [];
+        for(var i = 0; i < this.boundary.length; i++){
+            ret.push(this.centerPoint.segment(this.boundary[i], 1 - scale));
+        }
 
-Tile.prototype.getLatLon = function(radius, boundaryNum){
-    var point = this.centerPoint;
-    if(typeof boundaryNum == "number" && boundaryNum < this.boundary.length){
-        point = this.boundary[boundaryNum];
-    }
-    var phi = Math.acos(point.y / radius); //lat 
-    var theta = (Math.atan2(point.x, point.z) + Math.PI + Math.PI / 2) % (Math.PI * 2) - Math.PI; // lon
-    
-    // theta is a hack, since I want to rotate by Math.PI/2 to start.  sorryyyyyyyyyyy
-    return {
-        lat: 180 * phi / Math.PI - 90,
-        lon: 180 * theta / Math.PI
-    };
-};
-
-
-
-Tile.prototype.scaledBoundary = function(scale){
-
-    scale = Math.max(0, Math.min(1, scale));
-
-    var ret = [];
-    for(var i = 0; i < this.boundary.length; i++){
-        ret.push(this.centerPoint.segment(this.boundary[i], 1 - scale));
+        return ret;
     }
 
-    return ret;
-};
+    toJson(){
+        // this.centerPoint = centerPoint;
+        // this.faces = centerPoint.getOrderedFaces();
+        // this.boundary = [];
+        return {
+            centerPoint: this.centerPoint.toJson(),
+            boundary: this.boundary.map(function(point){return point.toJson()})
+        };
 
-Tile.prototype.toJson = function(){
-    // this.centerPoint = centerPoint;
-    // this.faces = centerPoint.getOrderedFaces();
-    // this.boundary = [];
-    return {
-        centerPoint: this.centerPoint.toJson(),
-        boundary: this.boundary.map(function(point){return point.toJson()})
-    };
+    }
 
+    toString(){
+        return this.centerPoint.toString();
+    }
 }
-
-Tile.prototype.toString = function(){
-    return this.centerPoint.toString();
-};
-
-export { Tile }
